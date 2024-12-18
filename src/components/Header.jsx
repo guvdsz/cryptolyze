119ff96008a2e29b8262ec02b012db476a1390b4
Gustavo Viana <167935951+guvdsz@users.noreply.github.com>
2024-12-18 12:23:58 -0300

    fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import OverviewCard from "./OverViewCard";
 export default function Header(props) {
   return (
     <header>
-      <h1>Cryptolyze</h1>
+      <h1>Cryptolyze <i class="fa-solid fa-chart-simple"></i></h1>
       <div className="overview-container">
         <OverviewCard value={`$${props.totalMarketCap}`} title="Total Market Cap" />
         <OverviewCard value={`$${props.totalVolume}`} title="Total Volume" />
